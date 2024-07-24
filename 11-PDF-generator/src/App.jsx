7181750dc71340662a64a113de7b1e036df47c12
VagnerNatvidade <vagnerflorindojunior@gmail.com>
2024-07-23 23:31:42 -0300

    first commit

---
+++ 11-PDF-generator/src/App.jsx
@@ -0,0 +1,12 @@
+import PdfGenerator from "./components/PdfGenerator";
+
+function App() {
+  return (
+    <div>
+      <h1>Gerador de PDF</h1>
+      <PdfGenerator />
+    </div>
+  );
+}
+
+export default App;
---
+++ 11-PDF-generator/src/components/TextStyleConfig.jsx
@@ -0,0 +1,41 @@
+function TextStyleConfig({
+  fontSize,
+  setFontSize,
+  fontColor,
+  setFontColor,
+  isBold,
+  setIsBold,
+}) {
+  return (
+    <div className="textStyleConfig">
+      <label className="configLabel">
+        Font Size:
+        <input
+          type="text"
+          className="input"
+          value={fontSize}
+          onChange={(e) => setFontSize(e.target.value)}
+        />
+      </label>
+      <label className="configLabel">
+        Font color:
+        <input
+          type="color"
+          className="colorPicker"
+          value={fontColor}
+          onChange={(e) => setFontColor(e.target.value)}
+        />
+      </label>
+      <label className="configLabel">
+        Bold:
+        <input
+          type="checkbox"
+          value={isBold}
+          onChange={(e) => setIsBold(e.target.value)}
+        />
+      </label>
+    </div>
+  );
+}
+
+export default TextStyleConfig;
